d7e1e4cdd322ff1d358ea838b0bb5c4ede120159
Stuart Williams <stuartw@magenic.com>
2018-06-22 15:51:57 -0700

    Refactoring to promise

--- Workshop/mongodb/index.js
+++ Workshop/mongodb/index.js
@@ -7,119 +7,133 @@ const dataFolder = 'seeddata';
 const userCollection = 'users';
 const postCollection = 'posts';
 
-var fs = require('fs');
+var fs = require('promise-fs');
 var path = require('path');
 var http = require('http');
 
 var mongo = require("mongodb");
 var mongoClient = require("mongodb").MongoClient;
 
-// This is the format of the configuration file (case sensitive)
-var _settings = {
-    "ConnectionString": "",
-    "Database": ""
-};
+fs.readFile(settingsFile, 'utf8')
+    .then(function (data) {
+        var settings = JSON.parse(data);
 
-fs.readFile(settingsFile, 'utf8', function (err, data) {
-    if (err) throw err;
+        console.log(settings.ConnectionString);
+        console.log(settings.Database);
 
-    _settings = JSON.parse(data);
-
-    console.log(_settings.ConnectionString);
-    console.log(_settings.Database);
-
-    global.settings = _settings;
-
-    setupMongo();
-    soVeryDone();
-});
+        setupMongo(settings).then(function () {
+            soVeryDone();
+        });
+    })
+    .catch(function (err) {
+        throw err;
+    })
 
 var soVeryDone = function () {
     // process.exit(0);
     process.exitCode = 0;
 }
 
-var setupMongo = function () {
-    
-        mongoClient.connect(settings.ConnectionString, function (err, db) {
-            if (err) throw err;
+var setupMongo = function (settings) {
 
-            // Switch DB from 'admin' (default) to the desired Database, this is far from obvious
+    mongoClient.connect(settings.ConnectionString)
+        .then(function (db) {
             db = db.db(settings.Database);
             console.log('Current DB: ' + db.databaseName);
-
-            // if there are no records read the data folder and make some
-            anyRecords(db, userCollection, makeUserTestData);
-            anyRecords(db, postCollection, makePostsTestData);
-        });
-
+            anyRecords(db, userCollection)
+            .then(function (flag) {
+                if(flag) makeUserTestData(db);
+            });
+            anyRecords(db, postCollection)
+            .then(function (flag) {
+                if(flag) makePostsTestData(db);
+            });
+        })
+        .catch(reject(err));
 }
 
-var anyRecords = function (db, collectionName, callback) {
-    db.listCollections().toArray(function (err, collections) {
-        if (collections.includes(collectionName)) {
-            db.collection(collectionName).find().toArray(function (err, items) {
-                if (err) throw err;
-                if (items) {
-                    if (items.length <= 0) {
-                        callback(db);
-                    }
+var anyRecords = function (db, collectionName) {
+    db.listCollections()
+        .then(function (list) {
+            list.toArray(function (err, collections) {
+                if (collections.includes(collectionName)) {
+                    db.collection(collectionName).find().toArray(function (err, items) {
+                        if (err) reject(err);
+                        if (items) {
+                            if (items.length <= 0) {
+                                resolve(true);
+                            }
+                        }
+                    });
+                } else {
+                    db.createCollection(collectionName, function (err, res) {
+                        if (err) reject(err);
+                        resolve(true);
+                    });
                 }
             });
-        } else {
-            db.createCollection(collectionName, function (err, res) {
-                if (err) throw err;
-                callback(db);
-            });
-        }
-    });
+        })
+        .catch(function (err) {
+            reject( err);
+        });
+        resolve(false);
 };
 
 var makeUserTestData = function (db) {
     var dataFile = path.join(__dirname, dataFolder, 'users.json');
-    fs.readFile(dataFile, 'utf8', function (err, data) {
-        if (err) throw err;
 
-        var users = JSON.parse(data);
-        users.forEach(function (item, index) {
-            db.collection(userCollection).insertOne(item, function (err, res) {
-                if (err) throw err;
-                console.log(res);
+    fs.readFile(dataFile, 'utf8')
+        .then(function (data) {
+            var users = JSON.parse(data);
+            users.forEach(function (item, index) {
+                db.collection(userCollection).insertOne(item, function (err, res) {
+                    if (err) reject(err);
+                    console.log(res);
+                });
             });
+        })
+        .catch(function (err) {
+            reject(err);
         });
-
-    });
 }
 
 var makePostsTestData = function (db) {
 
+    var dataFile = path.join(__dirname, dataFolder, 'posts.json');
+    
     var bucket = new mongo.GridFSBucket(db, {
         chunkSizeBytes: 1024,
         bucketName: 'images'
     });
 
-    var dataFile = path.join(__dirname, dataFolder, 'posts.json');
-    fs.readFile(dataFile, 'utf8', function (err, data) {
-        if (err) throw err;
-
-        var posts = JSON.parse(data);
-        posts.forEach(function (item, index) {
-
-            var imagePath = path.join(__dirname, dataFolder, item.image);
-            fs.createReadStream(imagePath)
-                .pipe(
-                    bucket.openUploadStream(item.image)
-                )
-                .on('error', function (error) {
-                    throw error;
-                })
-                .on('finish', function () {
-                    db.collection(postCollection).insertOne(item, function (err, res) {
-                        if (err) throw err;
-                        console.log(res);
-                    });
-                });
-
+    fs.readFile(dataFile, 'utf8')
+        .then(function (data) {
+            var posts = JSON.parse(data);
+            posts.forEach(function (item, index) {
+                makePost(db, item, bucket);
+            });
+        })
+        .catch(function (err) {
+           reject(err);
         });
-    });
+}
+
+
+var makePost = function(db, item, bucket) {
+    var imagePath = path.join(__dirname, dataFolder, item.image);
+
+    fs.createReadStream(imagePath)
+        .pipe(
+            bucket.openUploadStream(item.image)
+        )
+        .on('error', function (err) {
+            reject(err);
+        })
+        .on('finish', function () {
+            db.collection(postCollection).insertOne(item, function (err, res) {
+                if (err) reject(err);
+                console.log(res);
+                resolve(true);
+            });
+        });
 }
