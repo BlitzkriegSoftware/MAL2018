813fb0cbd7f4c67b54ea21247af5bfaaf3208046
Stuart Williams <stuartw@magenic.com>
2018-07-03 11:59:57 -0700

    Async

--- Workshop/mongodb/index.js
+++ Workshop/mongodb/index.js
@@ -4,8 +4,6 @@ require('make-promises-safe')
 
 var exitCode = 0;
 
-var settings = {};
-
 const settingsFile = './settings.json';
 const dataFolder = 'seeddata';
 
@@ -13,122 +11,83 @@ const userCollection = 'users';
 const postCollection = 'posts';
 
 var util = require('util');
-
 var fs = require('fs');
-var readFile = util.promisify(fs.readFile);
+const readFile = util.promisify(fs.readFile);
 
 var path = require('path');
 
 var mongo = require('mongodb');
 var mongoClient = require('mongodb').MongoClient;
 
-var db = null;
-
-readFile(settingsFile, 'utf8')
-    .then((data) => {
-        console.log(data);
-        settings = JSON.parse(data);
-        console.log(settings.ConnectionString);
-        console.log(settings.Database);
-        return settings;
-    })
-    .then((settings) => {
-        return openDB(settings);
-    })
-    .then((_db) => { 
-        db = _db
-        return _db;
-    })
-    .then((db) => {
-        return collectionExistsWithData(db, userCollection);
-    })
-    .then((flag) => {
-       console.log(`Flag: ${ flag }`);
-       if(flag) makeUserTestData();
-    })
-    .then((db) => {
-        return collectionExistsWithData(db, postCollection);
-    })
-    .then((flag) => {
-       console.log(`Flag: ${ flag }`);
-       if(flag) makePostsTestData(db);
-    })
-    .then(() => {
-        timeToGo(0);
-    })
-    .catch((err) => {
-        console.log( err );
-        timeToGo(1);
-    });
-
-// Useful Promises
-
-function openDB(settings) {
-    return new Promise((resolve, reject) => {
-        mongoClient.connect(settings.ConnectionString)
-            .then((_db) => {
-                _db = _db.db(settings.Database);
-                console.log('Current DB: ' + _db.databaseName);
-                resolve(_db);
-            })
-    })
+// Processing
+async function Main() {
+    var sData = await readFile(settingsFile, 'utf8');
+    var settings = JSON.parse(sData);
+    console.log(settings.ConnectionString);
+    console.log(settings.Database);
+    var db = await openDB(settings);
+    var flag1 = await collectionExistsWithData(db, userCollection);
+    if (flag1) {
+        await makeUserTestData(db);
+    }
+    var flag2 = await collectionExistsWithData(db, postCollection);
+    if (flag2) {
+        await makePostsTestData(db);
+    }
 }
 
-function collectionExistsWithData(db, collectionName) {
-    return new Promise((resolve, reject) => {
-        db.collection(collectionName).findOne({}, function(err, result) {
-            if (err) reject( err );
-            if(result) resolve(false);
-            else {
-                db.createCollection(collectionName, function (err, res) {
-                    if (err) reject(err);
-                    resolve(true);
-                });
-            }
-        });
-    })
-}
-
-// Helpers
-
-var timeToGo = function(exitCode) {
+Main().then(() => {
     process.exit(exitCode);
+});
+
+// f(x)
+
+async function openDB(settings) {
+    var _db = await mongoClient.connect(settings.ConnectionString);
+    _db = await _db.db(settings.Database);
+    console.log('Current DB: ' + _db.databaseName);
+    return _db;
 }
 
-var makeUserTestData = function (db) {
+async function collectionExistsWithData(db, collectionName) {
+    var flag = false;
+    var result = await db.collection(collectionName).findOne({});
+    if (!result) {
+        var res = await db.createCollection(collectionName);
+        flag = true;
+    }
+    return flag;
+}
+
+async function makeUserTestData(db) {
+    var res = {};
     var dataFile = path.join(__dirname, dataFolder, 'users.json');
-
-    readFile(dataFile, 'utf8')
-        .then((data) => {
-            var users = JSON.parse(data);
-            users.forEach(function (item, index) {
-                db.collection(userCollection).insertOne(item, function (err, res) {
-                    if (err) reject(err);
-                    console.log(res);
-                });
-            });
-        })
+    var data = await readFile(dataFile, 'utf8');
+    var users = JSON.parse(data);
+    users.forEach(function (item) {
+        res = await db.collection(userCollection).insertOne(item);
+        console.log(res);
+    });
+    return true;
 }
 
-var makePostsTestData = function (db) {
-
-    var dataFile = path.join(__dirname, dataFolder, 'posts.json');
-    
+async function makePostsTestData(db) {
     var bucket = new mongo.GridFSBucket(db, {
         chunkSizeBytes: 1024,
         bucketName: 'images'
     });
 
-    readFile(dataFile, 'utf8')
-        .then(function (data) {
-            var posts = JSON.parse(data);
-            posts.forEach(function (item, index) {
-                makePost(db, item, bucket);
-            });
-        })
+    var dataFile = path.join(__dirname, dataFolder, 'posts.json');
+    var data = await readFile(dataFile, 'utf8');
+    var posts = JSON.parse(data);
+    posts.forEach(function (item) {
+        await makePost(db, item, bucket);
+    });
+    return true;
 }
 
-var makePost = function(db, item, bucket) {
+async function makePost(db, item, bucket) {
+
     var imagePath = path.join(__dirname, dataFolder, item.image);
 
     fs.createReadStream(imagePath)
@@ -136,13 +95,11 @@ var makePost = function(db, item, bucket) {
             bucket.openUploadStream(item.image)
         )
         .on('error', function (err) {
-            reject(err);
+            throw err;
         })
         .on('finish', function () {
-            db.collection(postCollection).insertOne(item, function (err, res) {
-                if (err) reject(err);
-                console.log(res);
-                resolve(true);
-            });
+            var res = await db.collection(postCollection).insertOne(item);
+            console.log(res);
+            return true;
         });
 }
